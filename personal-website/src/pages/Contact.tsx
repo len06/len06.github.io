128daa9e042653006578bbebe889b789adcac602
l3n06 <wylt06@gmail.com>
2025-01-05 18:13:26 +0100

    Made small changes and added new styles

--- personal-website/src/pages/Contact.tsx
+++ personal-website/src/pages/Contact.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import ContactInfo from '../components/ContactInfo/ContactInfo.tsx';
+import '../styles/contact.css';
 
 function Contact(){
     return (
-    <h1>Contact</h1>
+    <>
+    <div className="contact-main">
+        <div className="contact-card-wrapper">
+        <ContactInfo className="contact-card"/>
+        </div>
+        <img src='/Contact_image/mail.jpg' alt="street_light" className="contact-img"></img>
+    </div>
+    </>
     )
 }
 
--- personal-website/src/pages/index.tsx
+++ personal-website/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import About from './About.tsx';
 import Contact from './Contact.tsx';
 import Home from './Home.tsx';
 import Projects from './Projects.tsx';
@@ -8,10 +7,6 @@ const pages = [
     {
         path:'/',
         element:<Home />
-    },
-    {
-        path:'/about',
-        element:<About />
     },
     {
         path:'/contact',
